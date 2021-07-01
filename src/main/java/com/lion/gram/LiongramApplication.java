5d8adca92fa25069999a3893bc0324b8d92ad478
DraWoo <mire2566@naver.com>
2021-07-01 22:35:26 +0900

    Initial commit

---
+++ src/main/java/com/lion/gram/LiongramApplication.java
@@ -0,0 +1,13 @@
+package com.lion.gram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiongramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiongramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lion/gram/LiongramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lion.gram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiongramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
